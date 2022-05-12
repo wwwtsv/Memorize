08565a10e6acd3cbb014e1a57542c62239095014
aleksejcvetkov <cai.cwet@gmail.com>
2022-05-12 23:34:47 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Алексей Цветков on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
